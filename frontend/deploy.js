afff661b8174cd6a1a5d9aac258d47bef7895bb3
mauriciolaser <mauriciocastrovaldez@gmail.com>
2025-04-02 13:05:05 +0300

    arreglado el deploy

--- frontend/deploy.js
+++ frontend/deploy.js
@@ -1,16 +1,17 @@
-require('dotenv').config({ path: '.env.production' });
+// deploy.js
+const { DEPLOY_FTP_USER, DEPLOY_FTP_PASSWORD, DEPLOY_FTP_HOST, DEPLOY_FTP_PORT, DEPLOY_FTP_PATH } = require('./config');
 const FtpDeploy = require('ftp-deploy');
 const ftpDeploy = new FtpDeploy();
 
 const config = {
-  user: process.env.DEPLOY_FTP_USER,
-  password: process.env.DEPLOY_FTP_PASSWORD,
-  host: process.env.DEPLOY_FTP_HOST,
-  port: process.env.DEPLOY_FTP_PORT,
+  user: DEPLOY_FTP_USER,
+  password: DEPLOY_FTP_PASSWORD,
+  host: DEPLOY_FTP_HOST,
+  port: DEPLOY_FTP_PORT,
   localRoot: __dirname + "/build",         // Carpeta donde se genera tu build de React
-  remoteRoot: process.env.DEPLOY_FTP_PATH, // Ruta remota en tu hosting
-  include: ["*", "**/*"],                  // Sube todo lo que hay en /build
-  deleteRemote: false,                     // Si quieres borrar los archivos viejos en el servidor, pon true
+  remoteRoot: DEPLOY_FTP_PATH,              // Ruta remota en tu hosting
+  include: ["*", "**/*"],                   // Sube todo lo que hay en /build
+  deleteRemote: false,                      // Si quieres borrar archivos viejos en el servidor, pon true
   forcePasv: true
 };
 
